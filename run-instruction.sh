48a62b774b6a2b3307ea12bf3f16efcc794cd974
Vlad2000Andrei <cursaru.vlad@gmail.com>
2023-09-25 18:24:39 +0200

    Wrapper script to fill in the template and request a model answer

--- run-instruction.sh
+++ run-instruction.sh
@@ -1,8 +1,33 @@
 MODEL_FOLDER=$1
+LANGUAGE=$2
+PROMPT=$3
+SCRIPT_NAME="instruction-runner.py"
 
+SCRIPT_PATH=$MODEL_FOLDER/$SCRIPT_NAME
+PROMPT_TAG="<PROMPT>"
+LANGUAGE_TAG="<LANG>"
+TMPOUT_FILE="./tmpout.txt"
 
-# run the model
-torchrun --nproc_per_node 1 $MODEL_FOLDER/example_instructions.py \
+# make a file for tmp output
+touch $TMPOUT_FILE
+
+# Move runner script into model folder so it can access the Llama Python modules
+cp ./$SCRIPT_NAME $SCRIPT_PATH
+
+# Fill prompt into script (this is cursed but also kinda the only automate-able way)
+sed -i "s/$PROMPT_TAG/$PROMPT/gi" $SCRIPT_PATH
+sed -i "s/$LANGUAGE_TAG/$LANGUAGE/gi" $SCRIPT_PATH
+
+# run the model with the script
+torchrun --nproc_per_node 1 $SCRIPT_PATH \
     --ckpt_dir $MODEL_FOLDER/CodeLlama-7b-Instruct/ \
     --tokenizer_path $MODEL_FOLDER/CodeLlama-7b-Instruct/tokenizer.model \
-    --max_seq_len 512 --max_batch_size 4
+    --max_seq_len 512 --max_batch_size 4 > $TMPOUT_FILE
+
+# Get the assistant's answer from the mess of output
+python3 ./extract-response.py $TMPOUT_FILE
+
+# remove the script and other temp files
+rm $SCRIPT_PATH
+rm $TMPOUT_FILE
+
