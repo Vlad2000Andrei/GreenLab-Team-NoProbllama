075778ff515d438ab1d4f31741d106c7c98c4e75
Vlad2000Andrei <cursaru.vlad@gmail.com>
2023-09-25 18:25:14 +0200

    Script to bulk-run prompts

---
+++ run-all.py
@@ -0,0 +1,31 @@
+from sys import argv
+from os import system, path
+from time import time
+
+if not len(argv) >= 3:
+    print("Incorrect number of arguments")
+    exit(-1)
+
+promptfile = argv[1]
+modeldir = argv[2]
+
+with open(promptfile) as file:
+    lines = [line for line in file]
+
+total = len(lines)
+current = 1
+
+for line in lines:
+    [language, prompt] = line.split(maxsplit=1)
+    print(f"########### Generating Answer {current}/{total}.")
+    cmd = f"./run-instruction.sh {modeldir} {language.strip()} \"{prompt.strip()}\""
+
+    if (argv[3] == "-f"):
+        if not path.isdir("./answers"):
+            system("mkdir ./answers/")
+        filename = f"./answers/{language}-prompt-{current}-{int(time())}.txt"
+        cmd = f"touch {filename}; {cmd} > {filename}"
+
+    print(f"Executing: '{cmd}'")
+    system(cmd)
+    current += 1
