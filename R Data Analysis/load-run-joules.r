fda6c91fc796e14b41f05bbbe93481079d17315a
Vlad2000Andrei <cursaru.vlad@gmail.com>
2023-10-25 13:07:45 +0200

    Updated to parse human files correctly

--- R Data Analysis/load-run-joules.r
+++ R Data Analysis/load-run-joules.r
@@ -2,12 +2,12 @@ load_data <- function(path_all_joules, output_dir, path_separator="/") {
   benchmarks = list.dirs(path_all_joules)[-1] # Exclude the first element of the list because that is the path_all_joules parent dir
   data = data.frame()
   
-  for (dir in benchmarks) {
-    files = list.files(dir)
-    benchmark_name = strsplit(tail(strsplit(dir, path_separator)[[1]], 1), " ")[[1]][1] # Magic to get the TS / CN / SR out of the path
+  for (subdir in benchmarks) {
+    files = list.files(subdir)
+    benchmark_name = strsplit(tail(strsplit(subdir, path_separator)[[1]], 1), " ")[[1]][1] # Magic to get the TS / CN / SR out of the path
     
     for (file in files) {
-      path_file = paste(c(dir, file), collapse=path_separator)
+      path_file = paste(c(subdir, file), collapse=path_separator)
       run_data = read.csv(path_file) 
       run_data = as.numeric(as.vector(run_data[1,])[-1]) #the other script writes it all weird so we need to transpose it weirdly
       
@@ -36,17 +36,30 @@ load_data <- function(path_all_joules, output_dir, path_separator="/") {
       else if (promptnr %in% temps_10) {
         temp = "1.0"
       }
-      else {
+    
+      
+      if (grepl("human", subdir, ignore.case = TRUE)) {
         temp = "N/A"
+        efficient = "N/A"
+        source = "human"
+        algo = paste(strsplit(file, '_')[[1]][4:5], collapse = " ")
+        lang = strsplit(strsplit(file, '_')[[1]][6], " ")[[1]][1]
+      }
+      else {
+        source = "llama"
       }
       
-      new_row = data.frame(algo=algo, lang=lang, efficient=efficient, temp=temp)
+      new_row = data.frame(src=source, algo=algo, lang=lang, efficient=efficient, temp=temp)
       new_row$data = list(run_data)
       data = rbind(data, new_row)
       
     }
   }
   return(data)
+}
+
+get_bench_data <- function(data = load_data(), src="llama", alg, lang, temp, eff) {
+  return(1) 
 }
 
 path_all_joules = 'C:/Users/cursa/OneDrive/Documente/Uni/Green Lab/GreenLab-Team-NoProbllama/R Data Analysis/Data Files/Run Joules'
